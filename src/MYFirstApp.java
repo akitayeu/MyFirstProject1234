4b32edf3d148d56b0fb6b10e15d59c6cbd468bc2
akitayeu <a.kitayeu@sam-solutions.com>
2022-04-29 10:09:54 +0300

    Test

---
+++ src/MYFirstApp.java
@@ -0,0 +1,10 @@
+public class MYFirstApp {
+    public static void main (String[] args)
+    {
+        //Simple App with Hello World
+        System.out.print("Hello World and good evening");
+        System.out.println("Hello World and good evening");
+        /* Finish*/
+        /**Test comments*/
+    }
+}
